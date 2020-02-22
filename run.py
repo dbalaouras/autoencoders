9eb5d01602e5427eb5faac8efa94de14c7c3eb6f
Dimitris Balaouras <dbalaouras@gmail.com>
2020-02-23 00:06:04 +0200

    Implementing data creation and autoencoder implementation

--- run.py
+++ run.py
@@ -4,6 +4,14 @@ import os
 import random
 from optparse import OptionParser
 
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+
 __author__ = "Dimi Balaouras"
 __copyright__ = "Copyright 2020, uth.gr"
 __license__ = "Apache License 2.0, see LICENSE for more details."
@@ -28,6 +36,7 @@ def create_dataset():
     """
     Create a artificial PHR dataset
     """
+    print("Now creating PHR dataset")
     # erase cli_options.features_export_file
     open(cli_options.features_export_file, 'w').close()
 
@@ -36,7 +45,7 @@ def create_dataset():
     genders = [0, 1]
     random.seed(1)
     for i in range(cli_options.size_of_phr):
-        age = random.choice(age_range)
+        # age = random.choice(age_range)
         gender = random.choice(genders)
         idc10_sampled_codes = random.sample(icd10_codes,
                                             random.randint(cli_options.min_cond_num, cli_options.max_cond_num))
@@ -53,20 +62,105 @@ def create_dataset():
             wr.writerow(features_list)
 
 
-def save_dataset_csv(dataset):
+def create_model(data):
     """
 
-    :param dataset:
-    :return:
+    :return: The Autoencoder model
     """
+
+    print("Now creating model")
+    input_dim = data.shape[1]  # 329 features
+    encoding_dim = 150
+
+    # 5 connected layers with 329, 150, 75, 75 and 329 neurons respectively
+    input_layer = Input(shape=(input_dim,))
+
+    encoder = Dense(encoding_dim, activation="tanh", activity_regularizer=regularizers.l1(10e-5))(input_layer)
+    encoder = Dense(int(encoding_dim / 2), activation="relu")(encoder)
+    decoder = Dense(int(encoding_dim), activation='tanh')(encoder)
+    decoder = Dense(input_dim, activation='relu')(decoder)
+
+    model = Model(inputs=input_layer, outputs=decoder)
+
+    # loss function and optimizer
+    model.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def train_model(model, train_data, validation_data, noise=None):
+    """
+    Train the model
+    """
+    print("Now training model")
+
+    nb_epoch = 100
+    batch_size = 32
+
+    checkpointer = ModelCheckpoint(filepath=cli_options.checkpoint_path, verbose=0, save_best_only=True)
+    tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
+    train_data_dis = train_data
+
+    for row in train_data:
+        print(row)
+
+    history = model.fit(train_data_dis, train_data,
+                        epochs=nb_epoch,
+                        batch_size=batch_size,
+                        shuffle=True,
+                        validation_data=validation_data,
+                        verbose=1,
+                        callbacks=[checkpointer, tensorboard]).history
+
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper right')
+
+    plt.show()
+
+
+def restore_model(model):
+    """
+    Restore the model from stored weights
+    TODO: error checking
+    """
+    model.load_weights(cli_options.checkpoint_path)
 
 
 def run():
     """
-    Runs the various exercises
-    :return: None
+    Runs the various operations
     """
-    create_dataset()
+    if cli_options.generate:
+        create_dataset()
+
+    # import data
+    data = pd.read_csv(cli_options.features_export_file)
+    pd.set_option('display.max_rows', data.shape[0] + 1)
+
+
+    # split train and test datasets, keep 20% for testing purposes
+    x_train, x_test, = train_test_split(data, test_size=0.2, random_state=42)
+
+    model = create_model(data)
+
+    if cli_options.restore_encoder:
+        restore_model(model)
+
+    if cli_options.train_encoder:
+        train_model(model, x_train, (x_test, x_test), noise=None)
+
+    x_test1, x_test2, = train_test_split(x_train, test_size=0.05, random_state=42)
+
+    encoded_data = model.predict(x_test1)
+    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    #     print(x_test1)
+    #     print(encoded_data)
+
+    loss, acc = model.evaluate(x_test1, x_test1, verbose=2)
+    print("Model, accuracy: {:5.2f}%".format(100 * acc))
 
 
 def parse_options():
@@ -81,6 +175,18 @@ def parse_options():
     parser = OptionParser(usage=usage, version=__version__, description=__description__)
 
     # Define available command line options
+
+    parser.add_option("-g", "--generate", action="store_true", dest="generate", default=False,
+                      help="Generate PHR data")
+
+    parser.add_option("-t", "--train", action="store_true", dest="train_encoder", default=False,
+                      help="Train Autoencoder")
+
+    parser.add_option("-r", "--restore", action="store_true", dest="restore_encoder", default=False,
+                      help="Restore Autoencoder")
+
+    parser.add_option("-c", "--checkpoint_path", action="store", dest="checkpoint_path", default="model.h5",
+                      help="Path to store model weights")
 
     parser.add_option("-d", "--icd_file", action="store", dest="icd_file", default="icd10.csv",
                       help="Path to icd10 data csv file")
